0267e6d69b5d92a9ea837b3a04f20b4d2277ab69
Steven Tracey <70534545+nevetS-718@users.noreply.github.com>
2020-12-30 18:34:16 -0500

    Update and rename ExampleMod.java to Main.java

---
+++ src/main/java/net/fabricmc/example/Main.java
@@ -0,0 +1,9 @@
+package tech.nevets.everythingvanilla.Main;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+	@Override
+	public void onInitialize() {
+	}
+}
